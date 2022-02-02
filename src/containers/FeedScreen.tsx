27389aab182e82006b6803f6db6f5a00f01d8ffe
Rameez Hussain <rameezh88@gmail.com>
2022-02-02 18:59:18 +0530

    Error handling + retry for data loading

--- src/containers/FeedScreen.tsx
+++ src/containers/FeedScreen.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
-import { Text, View } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native';
+import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
 import { useQuery } from 'react-query';
 import XiteService from '../services/XiteService';
 
@@ -16,10 +17,33 @@ const FeedScreen = () => {
   }, [data]);
 
   return (
-    <View>
-      <Text>{'Feed Screen'}</Text>
+    <View style={styles.container}>
+      {error && (
+        <TouchableWithoutFeedback style={styles.errorContent} onPress={refetch}>
+          <Text>Error loading items. Tap to try again</Text>
+        </TouchableWithoutFeedback>
+      )}
+      {data && <Text>{'Items loaded'}</Text>}
+      {isLoading && <ActivityIndicator style={styles.loader} size="large" />}
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column'
+  },
+  loader: {
+    marginTop: 20,
+    alignSelf: 'center'
+  },
+  errorContent: {
+    color: 'black',
+    padding: 30,
+    textAlign: 'center',
+    alignSelf: 'center'
+  }
+});
 
 export default FeedScreen;
