b7640dc15fbddefcc733cebfea85dee70919255f
Rameez Hussain <rameezh88@gmail.com>
2022-02-03 00:12:28 +0530

    Move SearchField to its own component

---
+++ src/components/SearchField.tsx
@@ -0,0 +1,53 @@
+import React, { useRef } from 'react';
+import { StyleSheet, TextInput, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import CrossIcon from '../assets/cross.svg';
+import { Colors } from '../styles/colors';
+
+export interface SearchFieldProps {
+  onChangeText: (text: string) => void;
+}
+
+const SearchField = (props: SearchFieldProps) => {
+  const { onChangeText } = props;
+  const searchInputRef = useRef<TextInput | null>(null);
+  return (
+    <View style={styles.searchContainer}>
+      <TextInput
+        ref={searchInputRef}
+        style={styles.searchField}
+        placeholderTextColor={Colors.metaGrey}
+        placeholder="Search by title or artist"
+        onChangeText={onChangeText}
+      />
+      <TouchableOpacity
+        style={styles.searchClearButton}
+        onPress={() => {
+          searchInputRef.current?.clear();
+        }}>
+        <CrossIcon size={30} fill="#000" />
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  searchContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  searchField: {
+    paddingLeft: 10,
+    width: '100%',
+    alignSelf: 'center'
+  },
+  searchClearButton: {
+    alignSelf: 'center',
+    height: 25,
+    width: 25
+  }
+});
+
+export default SearchField;
--- src/containers/SearchScreen.tsx
+++ src/containers/SearchScreen.tsx
@@ -1,35 +1,15 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React, { useLayoutEffect, useRef } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { TextInput, TouchableOpacity } from 'react-native-gesture-handler';
-import { RootStackParamsList } from 'src/navigation/RootNavigator';
-import Cross from '../assets/cross.svg';
-import { Colors } from '../styles/colors';
+import React, { useLayoutEffect } from 'react';
+import { Text, View } from 'react-native';
+import SearchField from '../components/SearchField';
+import { RootStackParamsList } from '../navigation/RootNavigator';
 
 type Props = NativeStackScreenProps<RootStackParamsList, 'Search'>;
 
 const SearchScreen = ({ navigation }: Props) => {
-  const searchInputRef = useRef<TextInput | null>(null);
-
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerTitle: () => (
-        <View style={styles.searchContainer}>
-          <TextInput
-            ref={searchInputRef}
-            style={styles.searchField}
-            placeholderTextColor={Colors.metaGrey}
-            placeholder="Search by title or artist"
-          />
-          <TouchableOpacity
-            style={styles.searchClearButton}
-            onPress={() => {
-              searchInputRef.current?.clear();
-            }}>
-            <Cross size={30} fill="#000" />
-          </TouchableOpacity>
-        </View>
-      )
+      headerTitle: () => <SearchField onChangeText={(text: string) => {}} />
     });
   }, [navigation]);
 
@@ -39,24 +19,5 @@ const SearchScreen = ({ navigation }: Props) => {
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  searchContainer: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  searchField: {
-    paddingLeft: 10,
-    width: '100%',
-    alignSelf: 'center'
-  },
-  searchClearButton: {
-    alignSelf: 'center',
-    height: 25,
-    width: 25
-  }
-});
 
 export default SearchScreen;
